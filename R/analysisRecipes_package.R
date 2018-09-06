033b25da6ab95e0fa216ae31eeeca0ba0c1b36ab
Naren Srinivasan <naren@Narens-MacBook-Pro.local>
2018-09-06 13:56:35 +0530

    Renamed package and and functions to comply with the name analysisPipelines. Also changed the getOutputByOrderId function, to return the output in the original class by default

--- R/analysisRecipes_package.R
+++
@@ -1,9 +0,0 @@
-#' analysisRecipes
-#'
-#' This package allows data scientists to compose and generate reports as a set of analytical operations.
-#' The sequence of generation can be stored as recipes and reused, specifically for production systems where these
-#' tasks are run repetitively. Additionally, the package implements a form of lazy evaluation where the recipes are
-#'  run on datasets only when outputs/ reports need to be generated
-#' @docType package
-#' @name analysisRecipes
-NULL
