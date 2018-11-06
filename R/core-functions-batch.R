cc1efac9fa934e2e26da860ce54e7f8b72f47b04
Naren Srinivasan <Naren.Srinivasan@mu-sigma.com>
2018-11-06 18:41:40 +0530

    PARTIAL COMMIT FOR ISSUES - #28. FULL COMMIT FOR ISSUES - #21, #3. Added logging, process time evaluation, as well as restructuring of execution mechanism for complex pipelines. PENDING | Update vignettes, test interoperable pipelines under the new design.

--- R/core-functions-batch.R
+++ R/core-functions-batch.R
@@ -203,63 +203,404 @@ checkSchema <- function(dfOld, dfNew){
     stop(paste0("All engines required for the pipelines have not been configured. ",
                 "Please use the analysisPipelines::assessEngine() function to check"))
   }
-  pipelineRegistryJoin <- dplyr::left_join(object@pipeline, object@registry, by = c("operation" = "functionName"))
 
-  if(nrow(pipelineRegistryJoin) > 0){
-    for(rowNo in 1:nrow(pipelineRegistryJoin)){
-
-      #Check engine
-
-      if(rowNo > 1){
-        prevEngine <- pipelineRegistryJoin[["engine"]][rowNo - 1]
-        currEngine <- pipelineRegistryJoin[["engine"]][rowNo]
-      }else{
-        prevEngine <- currEngine <-  pipelineRegistryJoin[["engine"]][rowNo]
-      }
-
-
-      ## Check outAsIn and engine conversion accordingly
-      if(pipelineRegistryJoin[['outAsIn.x']][rowNo] == T && rowNo > 1){
-        #object@workingInput <- do.call(object@pipeline[['operation']][[rowNo]], append(list(input), object@pipeline[['parameters']][[rowNo]]))
-        if(prevEngine != currEngine){
-
-          if(prevEngine == 'spark'){
-
-            if(currEngine == 'r'){
-              inputToExecute <- SparkR::as.data.frame(object@output[[rowNo-1]])
-            }
-
-          }else if(prevEngine == 'r'){
-            if(currEngine == "spark"){
-              inputToExecute <- SparkR::as.DataFrame(object@output[[rowNo-1]])
-            }
-
-          }
-        }else{
-          inputToExecute <- object@output[[rowNo-1]]
-        }
-      }else{
-
-        if(currEngine == 'r'){
-          inputToExecute <- object@input
-        }else if(currEngine == 'spark'){
-          inputToExecute <- SparkR::as.DataFrame(object@input)
-        }
-      }
-      object@output[[rowNo]] <- tryCatch({do.call(pipelineRegistryJoin[['operation']][[rowNo]],
-                                                  append(list(inputToExecute),
-                                                         pipelineRegistryJoin[['parameters']][[rowNo]]))},
-                                         error = function(e){
-                                           do.call(pipelineRegistryJoin[['exceptionHandlingFunction']][[rowNo]],
-                                                   list(error = e))
-                                         })
-    }
-  }else{
-    stop("No functions have been added to the pipeline")
+  if(nrow(object@pipelineExecutor$topologicalOrdering) == 0){
+    object %>>% prepExecution -> object
   }
+
+  object %>>% .executeByBatch -> object
 
   return(object)
 }
+
+#' @name .prepExecution
+#' @keywords internal
+.prepExecution <- function(object){
+  startPipelinePrep <- Sys.time()
+  flog.info("||  Pipeline Prep. STARTED  ||" , name='logger.prep')
+
+
+  object@pipeline$dependencies <- rep(NA, nrow(object@pipeline))
+  object@pipeline %>>% setUpstreamDependencies -> object@pipeline #Parents
+
+  pipelineRegistryJoin <- dplyr::left_join(object@pipeline, object@registry, by = c("operation" = "functionName"))
+  # pipelineRegistryJoin$dependencies <- rep(NA, nrow(pipelineRegistryJoin))
+  # pipelineRegistryJoin %>>% setDependencies -> pipelineRegistryJoin
+
+  pipelineRegistryJoin %>>% computeEdges -> edgeDf
+  pipelineRegistryJoin$id %>>% as.character %>>% getStartingPoints(edgeDf) -> startingPoints
+  nodes <- as.character(pipelineRegistryJoin$id)
+
+  topOrdering <- identifyTopologicalLevels(nodes, edgeDf)
+  object@pipelineExecutor$topologicalOrdering <- topOrdering
+  object@pipelineExecutor$dependencyLinks <- edgeDf
+
+  endPipelinePrep <- Sys.time()
+  prepTime <- endPipelinePrep - startPipelinePrep
+  flog.info("||  Pipeline Prep. COMPLETE. Time taken : %s seconds||", prepTime, name='logger.prep')
+
+  return(object)
+}
+
+setMethod(
+  f = "prepExecution",
+  signature = "BaseAnalysisPipeline",
+  definition = .prepExecution
+)
+
+.executeByBatch <- function(object){
+
+  startPipelineExecution <- Sys.time()
+  flog.info("||  Pipeline Execution STARTED  ||" , name='logger.execution')
+
+  outputCache <- new.env()
+
+  topOrder <- object@pipelineExecutor$topologicalOrdering
+  dplyr::left_join(object@pipeline, object@registry, by = c("operation" = "functionName")) %>>%
+    dplyr::left_join(object@pipelineExecutor$topologicalOrdering, by = c("id" = "id")) -> pipelineRegistryOrderingJoin
+
+  # pipelineRegistryJoin <- dplyr::left_join(object@pipeline, object@registry, by = c("operation" = "functionName"))
+  batches <- unique(pipelineRegistryOrderingJoin$level)
+  numBatches <- max(as.numeric(batches))
+
+  #   no_cores <- parallel::detectCores() - 1
+  #   cl <- parallel::makeCluster(no_cores)
+  #   parallel::clusterExport(cl = cl, envir = .GlobalEnv, varlist = ls())
+  # .executeBatch <- function(y, functionsInBatch){
+  #
+  #   return(NULL)
+  # }
+
+
+
+  # Iterate across batches
+  lapply(batches, function(x, object, pipelineRegistryOrderingJoin){
+
+    startBatch <- Sys.time()
+    pipelineRegistryOrderingJoin %>>% dplyr::filter(level == x) -> functionsInBatch
+    flog.info("||  Executing Batch Number : %s/%s containing functions '%s' ||",
+              x, numBatches, paste(functionsInBatch$operation, collapse = ", "),
+              name='logger.batch')
+
+    # Garbage cleaning in the cache - Previous batch outputs
+
+    prevBatch <- as.character(as.numeric(x) - 1)
+    pipelineRegistryOrderingJoin %>>% dplyr::filter(level == prevBatch) -> funcInPrevBatch
+
+    if(nrow(funcInPrevBatch)>0){
+      possiblePrevCacheOutputNames <- paste0("f", funcInPrevBatch$id, ".out")
+      previousCachedOutputNames <- intersect(possiblePrevCacheOutputNames, ls(outputCache))
+      pipelineRegistryOrderingJoin %>>% dplyr::filter(storeOutput == TRUE) -> requiredOutputs
+      requiredOutputs <-  paste0("f", requiredOutputs$id, ".out")
+
+      unrequiredCachedOutputNames <- setdiff(previousCachedOutputNames, requiredOutputs)
+
+      lapply(unrequiredCachedOutputNames, function(n){
+        rm(list = n, envir = outputCache)
+        return(NULL)
+      })
+      # object@pipelineExecutor$cache[unrequiredCachedOutputNames] <<- NULL
+
+      flog.info("||  Cleared intermediate outputs which are not required  ||", name = "logger.pipeline")
+
+    }
+
+    ## Function execution in a batch
+    lapply(functionsInBatch$id, function(y, object, functionsInBatch){
+      ## Check atleast one function to execute
+      ## Replace formula parameters with actual outputs
+      startFunc <- Sys.time()
+
+      functionsInBatch %>>% dplyr::filter(id == y) %>>% as.list -> funcDetails
+
+      flog.info("||  Function ID '%s' named '%s' STARTED on the '%s' engine ||",
+                funcDetails$id, funcDetails$operation, funcDetails$engine,
+                  name='logger.func')
+
+      # Set Input data
+      inputToExecute <- object@input
+
+      ###TODO: Change from outAsIn.x to outAsIn
+      if(funcDetails$outAsIn.x){
+        # inputToExecute <- object@pipelineExecutor$cache$workingInput
+        inputToExecute <- outputCache$workingInput
+      }
+
+      #Check engine
+      ### Python to be added
+
+      prevEngine <- "r"
+      currEngine <- funcDetails$engine
+      if(class(inputToExecute) == "SparkDataFrame"){
+        prevEngine <- "spark"
+      }
+
+      if(prevEngine != currEngine){
+        if(prevEngine == 'spark'){
+
+          if(currEngine == 'r'){
+            inputToExecute <- SparkR::as.data.frame(object@output[[rowNo-1]])
+          }
+
+        }else if(prevEngine == 'r'){
+          if(currEngine == "spark"){
+            inputToExecute <- SparkR::as.DataFrame(object@output[[rowNo-1]])
+          }
+
+        }
+      }else{
+        if(currEngine == 'spark'){
+          inputToExecute <- SparkR::as.DataFrame(inputToExecute)
+        }
+      }
+
+      # Set parameters
+
+      params <- unlist(funcDetails$parameters, recursive = F)
+      dep <- unlist(funcDetails$dependencies, recursive = F)
+      depTerms <- paste0("f", dep)
+
+      params <- lapply(params, function(p){
+        if(class(p) == "formula"){
+          formulaTerm <- attr(terms(p), "term.label")
+          if(length(formulaTerm) == 1 && formulaTerm %in% depTerms){
+            ## Formula of previous function in pipeline
+            actualParamObjectName <- paste0(formulaTerm, ".out")
+            p <-  unlist(object@pipelineExecutor$cache[[actualParamObjectName]], recursive = F)
+          }
+        }
+
+        return(p)
+      })
+
+
+      #Call
+
+      #Assign as named parameters
+      output <- tryCatch({do.call(what = funcDetails$operation,
+                                  args = append(list(inputToExecute),
+                                                params))},
+                         error = function(e){
+                           flog.error("||  ERROR Occurred in Function ID '%s' named '%s'. EXITING PIPELINE EXECUTION. Calling Exception Function - '%s'  ||",
+                                      funcDetails$id, funcDetails$operation, funcDetails$exceptionHandlingFunction,
+                                      name='logger.func')
+                           do.call(funcDetails$exceptionHandlingFunction,
+                                   list(error = e))
+
+                         })
+
+      ##outAsIn
+      if(funcDetails$outAsIn.x){
+        # object@pipelineExecutor$cache$workingInput <<- output
+        outputCache$workingInput <- output
+      }
+
+      opName <- paste0("f", funcDetails$id, ".out") #eg: f1.out
+      if(funcDetails$storeOutput){
+        assign(opName, value = list(output), envir = outputCache)
+        # object@pipelineExecutor$cache[[opName]] <<- list(output)
+      }else{
+        #Check if there are dependent children
+        fromList <- object@pipelineExecutor$dependencyLinks$from
+        if(funcDetails$id %in% fromList){
+          assign(opName, value = list(output), envir = outputCache)
+          # object@pipelineExecutor$cache[[opName]] <<- list(output)
+        }
+      }
+
+      endFunc <- Sys.time()
+      funcExecTime <- endFunc - startFunc
+
+      flog.info("||  Function ID '%s' named '%s' COMPLETED. Time taken : %s seconds  ||", funcDetails$id, funcDetails$operation, funcExecTime,
+                name='logger.func')
+
+    }, object, functionsInBatch)
+
+    # parallel::parLapply(X = functionsInBatch$id, cl = cl, function(y){s })
+    endBatch <- Sys.time()
+    batchExecTime <- endBatch - startBatch
+
+    flog.info("||  Batch Number %s/%s COMPLETE. Time taken : %s seconds  ||", x, numBatches, batchExecTime, name='logger.batch')
+
+  }, object, pipelineRegistryOrderingJoin)
+
+
+  flog.info("||  Performing final garbage cleaning and collection of outputs  ||",
+            name='logger.pipeline')
+
+  #Final garbage cleaning
+  pipelineRegistryOrderingJoin %>>% dplyr::filter(storeOutput == TRUE) -> requiredOutputs
+  requiredOutputs <-  paste0("f", requiredOutputs$id, ".out")
+
+  unrequiredCachedOutputNames <- setdiff(ls(outputCache), requiredOutputs)
+
+  lapply(unrequiredCachedOutputNames, function(n){
+    rm(list = n, envir = outputCache)
+    return(NULL)
+  })
+
+
+  object@output <- mget(ls(outputCache), envir = outputCache)
+  rm(list = ls(outputCache), envir = outputCache)
+  # stopCluster(cl)
+  # object@output <- object@pipelineExecutor$cache
+  #
+  # #Clear cache
+  # object@pipelineExecutor$cache <- NULL
+
+  endPipelineExecution <- Sys.time()
+  executionTime <- endPipelineExecution - startPipelineExecution
+
+  flog.info("||  Pipeline Execution COMPLETE. Time taken : %s seconds||", executionTime, name='logger.execution')
+  ##TODO: Change getOutputByOrderId function
+  return(object)
+}
+######################## Execution helper functions ############
+
+
+#' @name getUpstreamDependencies
+#' @keywords internal
+getUpstreamDependencies <- function(row){
+
+  ## dependencies from parameters
+  termRegexPattern <- "[f]|[:digit:]"
+  params <- row$parameters
+  dep <- lapply(params, function(p){
+    t <- NULL
+    tId <- NA
+    if(class(p) == "formula"){
+      t <- attr(terms(p), "term.labels")
+    }
+
+    isDependencyParam <- c()
+
+    if(!is.null(t)){
+      isDependencyParam <- grep(termRegexPattern, t)
+    }
+
+    if(length(isDependencyParam) > 0){
+      # Dependency param
+      tId <- as.numeric(gsub(pattern = "f", replacement = "", t))
+    }
+    return(tId)
+  })
+
+  ## Dependencies from outAsIn
+  if(row$outAsIn){
+    dep <- c(dep, as.character(as.numeric(row$id) - 1))
+  }
+
+
+  dep <- dep[which(!sapply(dep, is.na))]
+  dep <- paste(unique(dep), sep = ",")
+
+  return(dep)
+}
+
+
+#' @name setUpstreamDependencies
+#' @keywords internal
+setUpstreamDependencies <- function(pipeline){
+  pipeline %>>% apply(MARGIN = 1, FUN = getUpstreamDependencies) -> upstreamDependenciesList
+  # pipelineRegistryJoin %>>% dplyr::mutate(dependencies = dependenciesList) -> pipelineRegistryJoin
+  pipeline %>>% dplyr::mutate(dependencies = upstreamDependenciesList) -> pipeline
+  return(pipeline)
+}
+
+
+### Graph edges
+#' @name computeEdges
+#' @keywords internal
+computeEdges <- function(pipelineRegistryJoin){
+  edgesDf <- data.frame(from = c(),
+                        to = c())
+  pipelineRegistryJoin %>>% apply(MARGIN = 1, FUN = function(x, ...){
+    edges <- list()
+
+    if(length(x$dependencies) != 0){
+      id <- as.character(x$id)
+      parents <- unlist(strsplit(x$dependencies, ","))
+      edges <- lapply(parents, function(x, ...){
+        edge <- list(from = x, to = id)
+        return(edge)
+      }, id = id)
+
+      edges <- dplyr::bind_rows(edges)
+    }
+
+    return(edges)
+  }) %>>% dplyr::bind_rows(.) -> edgesDf
+
+  edgesDf %>>% dplyr::distinct(from, to, .keep_all = TRUE) -> edgesDf
+  return(edgesDf)
+}
+
+##Starting points
+#' @name getStartingPoints
+#' @keywords internal
+getStartingPoints <- function(nodes, edgeDf){
+  startingPoints <- setdiff(nodes, unique(edgeDf$to))
+  return(startingPoints)
+}
+
+### Topological levels
+
+#' @name identifyTopLevelRecursively
+#' @keywords internal
+identifyTopLevelRecursively <- function(input = list(topDf = dplyr::tibble(),
+                                                     nodes = c(),
+                                                     edgeDf = dplyr::tibble(),
+                                                     level = 1)){
+  topDf <- input$topDf
+  nodes <- input$nodes
+  edgeDf <- input$edgeDf
+  l <- input$level
+
+  if(nrow(edgeDf) == 0){
+    topDf %>>% dplyr::bind_rows(dplyr::bind_cols(id = nodes, level = rep(as.character(l), length(nodes)))) -> topDf
+    output <- list(topDf = topDf,
+                   nodes = nodes,
+                   edgeDf = edgeDf,
+                   level = l)
+    return(output)
+  }else{
+    startingPoints <- getStartingPoints(nodes, edgeDf)
+    topDf %>>% dplyr::bind_rows(dplyr::bind_cols(id = startingPoints, level = rep(as.character(l), length(startingPoints)))) -> topDf
+    edgeDf %>>% dplyr::filter(!(from %in% startingPoints)) -> edgeDf
+    nodes %>>% setdiff(startingPoints) -> nodes
+
+    output <- list(topDf = topDf,
+                   nodes = nodes,
+                   edgeDf = edgeDf,
+                   level = l + 1)
+    return(identifyTopLevelRecursively(output))
+  }
+}
+
+
+#' @name identifyTopologicalLevels
+#' @keywords internal
+identifyTopologicalLevels <- function(
+  nodes = c(),
+  edgeDf = dplyr::tibble(),
+  topDf = dplyr::tibble(id = character(),
+                        level = character()),
+  level = 1){
+  input <- list(topDf = topDf,
+                nodes = nodes,
+                edgeDf = edgeDf,
+                level = level)
+  topDf <- identifyTopLevelRecursively(input)$topDf
+  return(topDf)
+}
+
+
+
+
+
+
+
+
 
 #' @rdname generateOutput
 setMethod(
@@ -269,22 +610,22 @@ setMethod(
 )
 
 
-setMethod(
-  f = "generateOutput",
-  signature = "tbl",
-  definition = function(object)
-  {
-    input <- input
-    outList <- list()
-    for(rowNo in 1:nrow(object)){
-      if(object[['outAsIn']][rowNo] == T){
-        input <- do.call(object[['operation']][[rowNo]], append(list(input), object[['parameters']][[rowNo]]))
-      }
-      outList[[rowNo]] <- do.call(object[['operation']][[rowNo]], append(list(input), object[['parameters']][[rowNo]]))
-    }
-    return(outList)
-  }
-)
+# setMethod(
+#   f = "generateOutput",
+#   signature = "tbl",
+#   definition = function(object)
+#   {
+#     input <- input
+#     outList <- list()
+#     for(rowNo in 1:nrow(object)){
+#       if(object[['outAsIn']][rowNo] == T){
+#         input <- do.call(object[['operation']][[rowNo]], append(list(input), object[['parameters']][[rowNo]]))
+#       }
+#       outList[[rowNo]] <- do.call(object[['operation']][[rowNo]], append(list(input), object[['parameters']][[rowNo]]))
+#     }
+#     return(outList)
+#   }
+# )
 
 
 
