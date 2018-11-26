b098f0ef2338091817f56bf423afcd0118908786
Naren Srinivasan <Naren.Srinivasan@mu-sigma.com>
2018-11-26 20:48:44 +0530

    Removed SparkR from DESCRIPTION. Issue #15. Need to validate if this is the best approach.

--- R/analysisPipelines_package.R
+++ R/analysisPipelines_package.R
@@ -3,6 +3,9 @@
 #' The package aims at enabling data scientists to compose pipelines of analysis which consist of data manipulation,
 #' exploratory analysis & reporting, as well as modeling steps. It also aims to enable data scientists to use tools
 #' of their choice through an R interface, and compose interoperable pipelines between R, Spark, and Python.
+#'
+#' Important Note - This package uses 'SparkR' to interact with Spark and automatically installs it if not present
+#' from a Github repo as 'SparkR' is not distrubuted on CRAN
 #' @docType package
 #' @name analysisPipelines
 NULL
