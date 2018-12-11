fcde26ee2a7e3e896e1c1a44a8641d4af29d6f41
Naren Srinivasan <Naren.Srinivasan@mu-sigma.com>
2018-12-11 18:05:27 +0530

    Updated DESCRIPTION file and package description. Ready for CRAN release - v1.0.0

--- R/analysisPipelines_package.R
+++ R/analysisPipelines_package.R
@@ -3,9 +3,20 @@
 #' The package aims at enabling data scientists to compose pipelines of analysis which consist of data manipulation,
 #' exploratory analysis & reporting, as well as modeling steps. It also aims to enable data scientists to use tools
 #' of their choice through an R interface, and compose interoperable pipelines between R, Spark, and Python.
+#' Credits to Mu Sigma for supporting the development of the package.
 #'
-#' Important Note - This package uses 'SparkR' to interact with Spark and automatically installs it if not present
-#' from a Github repo as 'SparkR' is not distrubuted on CRAN
+#' @note To enable pipelines involving Spark tasks, the package uses the 'SparkR' package. Using Spark as an engine requires the SparkR package to be installed.
+#' SparkR is distributed natively with Apache Spark and is not distributed on CRAN. The SparkR version needs to directly map to the Spark version (hence the native distribution), and care needs to be taken to ensure that this is configured properly.
+#' To install from Github, run the following command, if you know the Spark version:
+#' \itemize{
+#' \item devtools::install_github('apache/spark@v2.x.x', subdir='R/pkg')
+#' }
+#' The other option is to install R by running the following terminal commands if Spark has already been installed:
+#' \itemize{
+#' \item $ export SPARK_HOME=/path/to/spark/directory
+#' \item $ cd $SPARK_HOME/R/lib/SparkR/
+#' \item $ R -e "devtools::install('.')"
+#' }
 #' @docType package
 #' @name analysisPipelines
 NULL
